6e7cdbb389272aba8d9e2566a010b0fe3a7b1cc9
Kanjamad Bosel <npor25@yahoo.com>
2019-01-28 16:23:11 -0800

    add classList

--- dom.js
+++ dom.js
@@ -1,5 +1,11 @@
 document.querySelector("li").setAttribute("random", "1000"); 
 // document.querySelector("h1").style.background="yellow";
 
+
+// className
 var h1 = document.querySelector("h1");
 h1.className = "coolTitle";
+
+
+// add class to li (classList)
+document.querySelector("li").classList.add("done");
