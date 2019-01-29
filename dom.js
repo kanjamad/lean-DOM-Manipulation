8984026c7f1ee17f7e7d6fca25272ce7e90279a6
Kanjamad Bosel <npor25@yahoo.com>
2019-01-28 16:13:30 -0800

    style h1 by add cooltitle

--- dom.js
+++ dom.js
@@ -1,2 +1,5 @@
-document.querySelector("li").setAttribute("random", "1000");
-document.querySelector("h1").style.background="yellow";
+document.querySelector("li").setAttribute("random", "1000"); 
+// document.querySelector("h1").style.background="yellow";
+
+var h1 = document.querySelector("h1");
+h1.className = "coolTitle";
