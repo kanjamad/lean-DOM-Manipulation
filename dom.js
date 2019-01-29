e08474780fc00f37b90309ab9b6bcd04fa2d4ef7
Kanjamad Bosel <npor25@yahoo.com>
2019-01-28 16:40:09 -0800

    classList.toggle

--- dom.js
+++ dom.js
@@ -10,4 +10,5 @@ h1.className = "coolTitle";
 // add class to li (classList)
 // document.querySelector("li").classList.add("coolTitle");
 document.querySelector("li").classList.remove("coolTitle");
+// classList.toggle => useful if I want to turn somthing on off back and forth
 document.querySelector("li").classList.toggle("done");
